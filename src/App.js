fc3855b638156a072f4f1df78d0bcbf8f01f0fc5
wlgus9 <diffn0409@gmail.com>
2023-01-13 13:44:39 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 웹사이트 화면에 대한 내용 출력을 담당
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // react는 기본적으로 jsx 문법을 따름
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Let's develop management system!</h2>
     </div>
   );
 }
